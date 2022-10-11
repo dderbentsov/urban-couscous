f903e7989b2bb3d963d7d182a707e6d10545031e
Daria Golova <danalis969@gmail.com>
2022-10-11 14:25:45 +0300

    Убрала scoped в App

--- src/App.vue
+++ src/App.vue
@@ -15,26 +15,26 @@
   }
 </script>
 
-<style lang="sass" scoped>
-#app
-  height: 100%
-  width: 100%
-  font-family: Raleway, sans-serif
-  font-style: normal
-  font-feature-settings: 'pnum' on, 'lnum' on
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-.pages-container
-  display: flex
-  flex: auto
-.app-container
-  height: 100%
-  width: 100%
-  row-gap: 8px
-  display: flex
-  flex-direction: column
-body
-  background-color: var(--bg-lavender-color)
-  margin: 0
-  padding: 0
+<style lang="sass">
+  #app
+    height: 100%
+    width: 100%
+    font-family: Raleway, sans-serif
+    font-style: normal
+    font-feature-settings: 'pnum' on, 'lnum' on
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+  .pages-container
+    display: flex
+    flex: auto
+  .app-container
+    height: 100%
+    width: 100%
+    row-gap: 8px
+    display: flex
+    flex-direction: column
+  body
+    background-color: var(--bg-lavender-color)
+    margin: 0
+    padding: 0
 </style>
