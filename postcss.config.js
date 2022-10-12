1c3439d4cabd3a06eedeb2b44343b55b28b1d800
DwCay <duminalexei@gmail.com>
2022-10-12 11:16:36 +0300

    Изменены настройки postcss, удалены лишние зависимости

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const tailwind = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const tailwindConfig = require("./tailwind.config");
-
-module.exports = {
-  plugins: [tailwind(tailwindConfig), autoprefixer],
-};
