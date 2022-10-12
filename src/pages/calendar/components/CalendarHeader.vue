cc50b4c6ffa18806568509b5ff9e50bf038ecd74
Daria Golova <danalis969@gmail.com>
2022-10-12 16:10:10 +0300

    Сделала переключатель День/Неделя

--- src/pages/calendar/components/CalendarHeader.vue
+++ src/pages/calendar/components/CalendarHeader.vue
@@ -1,38 +1,44 @@
 <template lang="pug">
   .calendar-header-wrapper.flex.items-center.justify-between.ml-2.py-3.pl-5.pr-6
-    .right-side.flex
+    .flex
       base-arrow-button.left-arrow.mr-4
       base-arrow-button.right-arrow.mr-6
       .text.flex.items-center
         span.font-medium.text-base 24 Мая 2022 
           span.today.font-bold.text-xxs(v-if="isToday") Сегодня
+    calendar-layout-switch(@selected="changeSelectedLayout")
 </template>
 
 <script>
 import BaseArrowButton from "@/components/base/buttons/BaseArrowButton.vue";
+import CalendarLayoutSwitch from "./CalendarLayoutSwitch.vue";
 export default {
   name: "CalendarHeader",
-  components: { BaseArrowButton },
+  components: { BaseArrowButton, CalendarLayoutSwitch },
   data() {
     return {
       isToday: true,
+      selectedLayout: "",
     };
   },
-  computed: {},
+  methods: {
+    changeSelectedLayout(option) {
+      this.selectedLayout = option;
+    },
+  },
 };
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 .calendar-header-wrapper
   width: 100%
   background-color: var(--bg-white-color)
   height: 56px
   border-radius: 4px
 
-.right-side
-
 .left-arrow
   transform: rotate(90deg)
+
 .right-arrow
   transform: rotate(270deg)
 
