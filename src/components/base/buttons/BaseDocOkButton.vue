90ac0e2535f66ce5cb4be32fa57ef28c02dfa926
Daria Golova <danalis969@gmail.com>
2022-10-13 17:54:59 +0300

    Сделала header CalendarColumn

---
+++ src/components/base/buttons/BaseDocOkButton.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  button.icon-wrap.flex.items-center.justify-center.py-1.px-2(:class="{disable: isDisable}")
+    span(v-if="numberIsVisible") 3
+    .icon-doc-ok.text-xxl
+</template>
+
+<script>
+export default {
+  name: "DocOkButton",
+  data() {
+    return {
+      numberIsVisible: false,
+      isDisable: true,
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.disable
+  opacity: 0.5
+
+.icon-wrap
+  height: 32px
+  background-color: var(--btn-blue-color-1)
+  color: var(--btn-blue-color)
+  border-radius: 4px
+  &:hover
+    background-color: var(--btn-blue-color-2)
+  &:active
+    background-color: var(--font-dark-blue-color)
+    color: var(--default-white)
+
+.icon-doc-ok
+  width: 24px
+  height: 24px
+</style>
---
+++ src/pages/calendar/components/CalendarColumn.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .calendar-column-wrapper
+    .header.flex.items-center.justify-between.py-2.px-6
+      .flex.items-center
+        img.avatar-wrapper.mr-2(:src="info.avatar" alt="Team member")
+        span.member-name.font-medium.text-base.mr-6 {{ info.name }}
+        img.icon-wrapper(src="@/assets/icons/lock.svg")
+      base-doc-ok-button  
+</template>
+<script>
+import BaseDocOkButton from "@/components/base/buttons/BaseDocOkButton.vue";
+export default {
+  name: "CalendarColumn",
+  components: { BaseDocOkButton },
+  props: {
+    info: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.calendar-column-wrapper
+  width: 100%
+  background-color: var(--default-white)
+
+.header
+  height: 48px
+
+.avatar-wrapper
+  width: 32px
+  height: 32px
+
+.icon-wrapper
+  width: 24px
+  height: 24px
+
+.member-name
+  color: var(--font-dark-blue-color)
+</style>
