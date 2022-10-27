cc84f4e26dc289913e325dffb7e31788393dd0a1
megavrilinvv <vastre@rambler.ru>
2022-10-27 17:43:23 +0300

    Удалил HomePage

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import TheHome from "@/pages/home/TheHome.vue";
 import TheCalendar from "@/pages/calendar/TheCalendar";
 import TheUser from "@/pages/clients/TheClients";
 import TheSettings from "@/pages/settings/TheSettings";
@@ -7,8 +6,7 @@ import TheSettings from "@/pages/settings/TheSettings";
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: "/", component: TheHome },
-    { path: "/calendar", component: TheCalendar },
+    { path: "/", component: TheCalendar },
     { path: "/clients", component: TheUser },
     { path: "/settings", component: TheSettings },
   ],
