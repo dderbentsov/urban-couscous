95dee21e3617bc96c4a535c8b120732b056722a1
Daria Golova <danalis969@gmail.com>
2022-11-08 17:40:59 +0300

    Изменила позиционирование формы создания клиента

--- src/components/base/BaseClientFormCreate.vue
+++ src/components/base/BaseClientFormCreate.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .wrapper-create.flex.flex-col.absolute.top-28.px-4.py-7.gap-y-8
+  .wrapper-create.flex.flex-col.absolute.top-0.px-4.py-7.gap-y-8
     .icon-cancel.close.absolute.top-5.right-5.cursor-pointer(@click="closeForm")
     span.title.text-xl.font-bold.px-4 Создание клиента
     .flex.gap-x-4.h-fit.px-4
